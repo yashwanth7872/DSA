f95f32a5bbe7e4615ab1ac0bff9a4e907144d2c2
yashwanth7872 <yashwanthm7872@gmail.com>
2021-08-25 07:17:16 +0530

    Java Sample test

---
+++ Java/Code.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Code
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        try{
+        x = Integer.parseInt(args[0]);
+        }
+        catch(Exception e){
+        x = 0;
+        }
+        
+        System.out.println("Yashwanth" + x);
+    }
+}
--- file.cpp
+++ file.cpp
@@ -4,36 +4,11 @@
 using namespace std;
 
 
-int lis(vector<int> &A)
-{
-    int maxi = INT_MIN;
-    int n = A.size();
-
-    for(int i = 0;i<n;i++)
-    {
-        for(int j = 0;j<n;j++)
-        {
-            if(i==j) 
-            {
-                maxi = max(maxi,0);
-            }
-            else
-            {
-                int ans = abs(A[i] - A[j]) + abs(i-j);
-                maxi = max(ans,maxi);
-            }
-        }
-    }
-
-    return maxi;
-}
-
-
 int main()
 {
     TIMESTART
-    vector<int>  arr = {1, 3, -1};
-    cout<<lis(arr);
+
+    cout<<"Good Morning";
 
     TIMEEND
     return 0;
